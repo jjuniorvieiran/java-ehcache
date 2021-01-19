8b3094ccfea1e931a524e6c67d309dea3706e32a
Junior <junior.jaava@gmail.com>
2021-01-19 19:15:27 +0100

    pay attention in NumberService.java

---
+++ demo-ehcache/src/main/java/com/example/demoehcache/DemoEhcacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoehcache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEhcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoEhcacheApplication.class, args);
+	}
+
+}
---
+++ demo-ehcache/src/test/java/com/example/demoehcache/DemoEhcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoehcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEhcacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
